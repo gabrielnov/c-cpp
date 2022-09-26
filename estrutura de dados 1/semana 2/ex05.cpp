f4da612513973a7e5a116666d04c91919208956f
Gabriel <gc.novaes@outlook.com>
2022-09-26 18:23:51 -0300

    adicionando novo ex

--- estrutura de dados 1/semana 2/ex05.cpp
+++ estrutura de dados 1/semana 2/ex05.cpp
@@ -9,9 +9,9 @@ class Complexo{
         Complexo(){};
         Complexo(float real, float imaginario);
         ~Complexo(){cout << "Número destruído." << endl;};
-        Complexo soma(Complexo c1, Complexo c2);
-        Complexo subtracao(Complexo c1, Complexo c2);
-        Complexo multiplicacao(Complexo c1, Complexo c2);
+        Complexo soma(Complexo c2);
+        Complexo subtracao(Complexo c2);
+        Complexo multiplicacao(Complexo c2);
 
 };
 
@@ -20,30 +20,30 @@ Complexo::Complexo(float r, float i){
     imaginario =i;
 }
 
-Complexo Complexo::soma(Complexo c1, Complexo c2){
+Complexo Complexo::soma(Complexo c2){
   Complexo r;
 
-  r.real = c1.real + c2.real;
-  r.imaginario = c1.imaginario + c2.imaginario;
+  r.real = this->real + c2.real;
+  r.imaginario = this->imaginario + c2.imaginario;
 
   return r;
 }
 
-Complexo Complexo::subtracao(Complexo c1, Complexo c2){
+Complexo Complexo::subtracao(Complexo c2){
   Complexo r;
 
-  r.real = c1.real - c2.real;
-  r.imaginario = c1.imaginario - c2.imaginario;
+  r.real = this->real - c2.real;
+  r.imaginario = this->imaginario - c2.imaginario;
 
   return r;
 }
 
 
-Complexo Complexo::multiplicacao(Complexo c1, Complexo c2){
+Complexo Complexo::multiplicacao(Complexo c2){
   Complexo r;
 
-  r.real = (c1.real * c2.real) - (c1.imaginario * c2.imaginario);
-  r.imaginario = (c1.real * c2.imaginario) + (c2.real * c1.imaginario);
+  r.real = (this->real * c2.real) - (this->imaginario * c2.imaginario);
+  r.imaginario = (this->real * c2.imaginario) + (c2.real * this->imaginario);
 
   return r;
 }
@@ -54,8 +54,8 @@ int main(){
     Complexo c1 (5, 5);
     Complexo c2 (2, 3);
     
-    Complexo c3 = c1.soma(c1, c2);
-    Complexo c4 = c1.subtracao(c1, c2);
+    Complexo c3 = c1.soma(c2);
+    Complexo c4 = c1.subtracao(c2);
 
     cout << "Soma: " << c3.real << " + " << c3.imaginario << "i" << endl;
     cout << "subtracao: " << c4.real << " + " << c4.imaginario << "i" << endl;
